3e3e9164e408b4b0caaab86415aba17d7d7d8977
xiangleilei <leilei.xiang@lantaiyuan.com>
2017-04-14 11:49:45 +0800

    重要部件维修记录

---
+++ extend_addons/repair_import_product_record/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': '重要部件的维修记录',
+    'version': '1.0',
+    'category': 'lty',
+    'summary': '重要部件管理',
+    'author': 'Xiang',
+    'description': """
+    重要部件的维修记录
+    """,
+    'data': [
+        'views/import_product_view.xml',
+    ],
+    'depends': ['fleet_manage_maintain', 'import_product'],
+}
