65bd07a6f289d5890caade22f3237f3f244a7d35
645530762@qq.com <645530762@qq.com>
2017-04-20 15:56:34 +0800

    计算在库数量

--- extend_addons/fleet_important_component/models/models.py
+++ extend_addons/fleet_important_component/models/models.py
@@ -60,6 +60,30 @@ class fleet_product(models.Model):
      #零件列表
      parts_ids = fields.One2many('product.parts_list', 'product_id')
 
+     @api.depends('qty_available')
+     def _compute_number_of_libraries(self):
+          """
+               根据在手数量，计算出在库数量
+               在库数量 = 在手数量 - 随车数量
+          :return:
+          """
+          for product in self:
+               #取重要部件
+               if product.important_type == 'component':
+                    print '进来..'
+                    domain = ['&',('product_id', '=', product.id),('parent_vehicle','!=',None)]
+                    count = self.env['product.component'].search_count(domain)
+                    print count
+                    product.number_of_libraries = product.qty_available - count
+          return self
+
+     #在库数量
+     number_of_libraries = fields.Float('Number of Libraries',compute=_compute_number_of_libraries)
+
+
+
+
+
 
 class fleet_component(models.Model):
 
