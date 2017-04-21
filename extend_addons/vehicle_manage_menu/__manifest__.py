41d7521f7a8f288e66ea9e54c7ec0379517f6380
xiangleilei <leilei.xiang@lantaiyuan.com>
2017-04-21 17:17:48 +0800

    重构

---
+++ extend_addons/vehicle_manage_menu/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "vehicle_manage_menu",
+
+    'description': """
+        车辆管理菜单项
+    """,
+
+    'author': "Xiangll",
+
+    'category': 'Basic Edition',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+         'views/menu_view.xml',
+    ],
+    # only loaded in demonstration mode
+    'installable': True,
+    'application': True,
+    'auto_install': False,    
+}
