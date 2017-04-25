ded91215a0112d4ad77d69b3d5c24e58e0f90e25
tuxinglong <xianglong.tu@lantaiyuan.com>
2017-04-25 18:57:45 +0800

    车辆年检 初步

--- extend_addons/fleet_vehicle_usage_management/models/models.py
+++ extend_addons/fleet_vehicle_usage_management/models/models.py
@@ -2,15 +2,17 @@
 
 from odoo import models, fields, api, _
 import time
-
+from odoo.exceptions import ValidationError
+import  datetime
+import odoo.tools.misc
 class FleetVehicle(models.Model):
     """
     车辆档案
     """
     _inherit = "fleet.vehicle"
 
+    # 是否报警
     warnning = fields.Boolean(compute="_needwarnning")
-
     def _needwarnning(self):
         for item in self:
             d = item.annual_inspection_date
@@ -25,41 +27,45 @@ class FleetVehicle(models.Model):
             else:
                 item.warnning = False
 
-
 class InspectionPlan(models.Model):
     _name = 'fleet_vehicle_usage_management.inspectionplan'
 
     # 年检计划名称
-    planname = fields.Char(string=_('inspection plan name'))
+    name = fields.Char(string=_('inspection plan name'), required=True)
     # 开始日期
-    startdate = fields.Date(string=_('inspection start date'))
+    startdate = fields.Date(string=_('inspection start date'), required=True)
     # 结束日期
-    endtdate = fields.Date(string=_('inspection end date'))
+    enddate = fields.Date(string=_('inspection end date'), required=True)
     # 制表日期
     create_date = fields.Datetime(string=_('plan create date'))
     # 审批时间
-    approvaldate = fields.Date(string=_('approval date'))
+    approvaldate = fields.Datetime(string=_('approval date'))
     # 分公司
-    branchcompany = fields.Many2one('hr.department', string=_('branch company'))
+    branchcompany = fields.Many2one('hr.department', string=_('branch company'), required=True)
     # 审批人
-    approver = fields.Many2one('res.user', string=_('approver'))
+    approver = fields.Many2one('res.users', string=_('approver'))
     # 制表人
-    maker = fields.Many2one('res.user', string=_('maker'))
+    maker = fields.Many2one('res.users', string=_('maker'), default=lambda self: self._uid)
+
     # 备注
     remark = fields.Char(string=_('remark'))
     # 计划详情
-    planitem_id = fields.One2many('fleet_vehicle_usage_management.planitem', 'vehicle_id',
+    planitem_id = fields.One2many('fleet_vehicle_usage_management.planitem', 'inspectionplan_id',
                                   string=_("plan detail"))
+    # 年检计划
+    subject = fields.Char(string=_('plan subject'))
+
     # 年检负责人
     principal = fields.Many2one('hr.employee', string=_('inspection principal'))
 
+    # 状态表
     state = fields.Selection([
-        ('draft',_('draft')),
-        ('submitted',_('submitted')),
-        ('checked',_('checked')),
-        ('execution',_('execution')),
-        ('done',_('done')),
-    ], string=_('inspection plan state'))
+        ('draft',_('draft')),           # 草稿
+        ('submitted',_('submitted')),   # 已提交
+        ('checked',_('checked')),       # 已审批
+        ('execution',_('execution')),   # 执行中
+        ('done',_('done')),             # 已完成
+    ], string=_('inspection plan state'),default='draft')
 
     @api.multi
     def action_draft(self):
@@ -72,6 +78,8 @@ class InspectionPlan(models.Model):
     @api.multi
     def action_checked(self):
         self.state = 'checked'
+        self.approver = self._uid
+        self.approvaldate = datetime.datetime.now().strftime(odoo.tools.misc.DEFAULT_SERVER_DATETIME_FORMAT)
 
     @api.multi
     def action_execution(self):
@@ -80,17 +88,108 @@ class InspectionPlan(models.Model):
     @api.multi
     def action_done(self):
         self.state = 'done'
-        self.boss = self._uid
 
+    @api.multi
     def generatePlanDetail(self):
         """
         自动填充计划详情列表
-        :return:
         """
-        pass
+        vehiclemode = self.env['fleet.vehicle']
+        for item in self:
+            startdate = item.startdate
+            enddate = item.enddate
+            externdate = time.strftime("%Y-%m-%d",
+                                       time.localtime(time.mktime(time.strptime(enddate, "%Y-%m-%d")) + 7776000))
 
-class planItem(models.Model):
+            vehiclelist = vehiclemode.search(
+                [
+                    ('annual_inspection_date', '>=', startdate),
+                    ('annual_inspection_date', '<=', externdate)
+                ]
+            )
+            items = []
+            for v in vehiclelist:
+                if self.hasItem(v.id)>0:
+                    continue
+                value = {
+                    'state' : 'execution',
+                    'vehicle_id' :v.id
+                }
+                items.append((0,0,value))
+            item.write({'planitem_id':items})
+        return True
+
+    # 已添加年检单的车辆不能再次被加入到计划单
+    def hasItem(self, vehicleid):
+        planitemmode = self.env['fleet_vehicle_usage_management.planitem']
+        count = planitemmode.search_count([
+            ('state','=','execution'),
+            ('vehicle_id','=',vehicleid)
+        ])
+        return count
+
+    @api.model
+    def create(self, vals):
+        res = super(InspectionPlan, self).create(vals)
+        planItems = vals.get('planitem_id', [])
+        for item in planItems:
+            if item[0] == 0 or item[0] == 1:
+                if self.hasItem(item[2]['vehicle_id']) > 1:
+                    raise ValidationError(_("there is a vehicle that already have inspection plan "))
+        return res
+
+    @api.multi
+    def write(self, vals):
+        res = super(InspectionPlan, self).write(vals)
+        planItems = vals.get('planitem_id', [])
+        for item in planItems:
+            if item[0] == 0 or item[0] == 1:
+                if self.hasItem(item[2]['vehicle_id'])>1:
+                    raise ValidationError(_("there is a vehicle that already have inspection plan "))
+        return res
+
+
+class PlanItem(models.Model):
+
     _name = 'fleet_vehicle_usage_management.planitem'
+
+
+    # 车辆信息
+    vehicle_id = fields.Many2one('fleet.vehicle', string=_('vehicle info'), required=True)
+
+    # 内部编号
+    inner_code = fields.Char(related='vehicle_id.inner_code', readonly=True)
+    # 车牌号
+    license_plate = fields.Char(related='vehicle_id.license_plate', readonly=True)
+    # 隶属公司
+    company_id = fields.Many2one(related='vehicle_id.company_id', readonly=True)
+    # 线路
+    route_id = fields.Many2one(related='vehicle_id.route_id', readonly=True)
+    # 年检到期
+    annual_inspection_date = fields.Date(related='vehicle_id.annual_inspection_date', readonly=True)
+    # 状态表
+    state = fields.Selection([
+        ('execution', _('execution')),  # 进行中
+        ('done', _('done')),            # 已完成
+    ], string=_('plan item state'), default='execution')
+
+    # 年检计划
+    inspectionplan_id = fields.Many2one('fleet_vehicle_usage_management.inspectionplan',
+                                        string=_('inspection plan'),ondelete='cascade')
+
+    # 送检司机
+    inspectiondriver = fields.Char(string=_('inspection driver'))
+    # 计划日期
+    plandate = fields.Date(string=_('plan to inspection date'))
+    # 实际年检日期
+    actualdate = fields.Date(string=_('actual inspection date'))
+
+class InspectionRecords(models.Model):
+    """
+    年检结果表
+    """
+
+    _name = 'fleet_vehicle_usage_management.inspectionrecords'
 
     # 年检执行日期
     inspectiondate = fields.Date(string=_('inspection date'))
@@ -98,8 +197,21 @@ class planItem(models.Model):
     inspectionexpire = fields.Date(string=_('inspection expire'))
     # 备注
     inspectionremark = fields.Date(string=_('inspection remark'))
-    # 年检计划
-    inspectionplan_id = fields.Many2one('fleet_vehicle_usage_management.inspectionplan',
-                                        string=_('inspection plan'))
+
     # 车辆信息
     vehicle_id = fields.Many2one('fleet.vehicle', string=_('vehicle info'))
+
+    # 内部编号
+    inner_code = fields.Char(related='vehicle_id.inner_code', readonly=True)
+    # 车牌号
+    license_plate = fields.Char(related='vehicle_id.license_plate', readonly=True)
+    # 登记证号
+    reg_no = fields.Char(related='vehicle_id.reg_no', readonly=True)
+    # 登记日期
+    reg_date = fields.Date(related='vehicle_id.reg_date', readonly=True)
+    # 强制报废
+    forced_destroy = fields.Char(related='vehicle_id.forced_destroy', readonly=True)
+    # 隶属公司
+    company_id = fields.Many2one(related='vehicle_id.company_id', readonly=True)
+    # 线路
+    route_id = fields.Many2one(related='vehicle_id.route_id', readonly=True)
