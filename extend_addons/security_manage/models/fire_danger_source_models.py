5dd85e2af40801f206b4bc2f907db5d92668a56b
chenfenghua <fenghua.chen@lantaiyuan.com>
2017-05-06 18:23:07 +0800

    添加了翻译

--- extend_addons/security_manage/models/fire_danger_source_models.py
+++ extend_addons/security_manage/models/fire_danger_source_models.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api,_
 
 
 class fire_danger_source_manage(models.Model):
     _name = 'sfs.fire_danger_src'
-    # 危险源名称
-    name = fields.Char()
+    # 危险源名称 string=_('')
+    name = fields.Char(string=_('source of dange name'))
     # 区域
-    area = fields.Char()
+    area = fields.Char(string=_('area'))
     # 位置
-    place = fields.Char()
+    place = fields.Char(string=_('place'))
     # 危害性
-    risk_evaluate = fields.Char()
+    risk_evaluate = fields.Char(string=_('harmful'))
     # 危害描述
-    danger_desc = fields.Char()
+    danger_desc = fields.Char(string=_('harmful description'))
     # 防范措施
-    precautions = fields.Char()
+    precautions = fields.Char(string=_('preventive measures'))
--- extend_addons/security_manage/models/repair_quality_models.py
+++ extend_addons/security_manage/models/repair_quality_models.py
@@ -5,18 +5,17 @@ from odoo import models, fields, api, _
 
 class repair_quality(models.Model):
     _name = 'srp.repair_quality'
-    name = fields.Char()
+    name = fields.Char(string=_('archive name'))
 
-    archives_class_big = fields.Many2one('security_manage.cls_manage', ondelete='set null',
+    archives_class_big = fields.Many2one('security_manage.cls_manage', string=_('archive big class'),
+                                         ondelete='set null',
                                          domain=[('class_type', '=', 'big_class')])
-    archives_class_little = fields.Many2one('security_manage.cls_manage', ondelete='set null',
+    archives_class_little = fields.Many2one('security_manage.cls_manage', string=_('archive little class'),
+                                            ondelete='set null',
                                             domain=[('class_type', '=', 'little_class')])
 
-    archvies_id = fields.Char(string=_('档案编号'), required=True, index=True,
+    archvies_id = fields.Char(string=_('archive id'), required=True, index=True,
                               copy=False, default=' ', readonly=True)
-    archives_creator = fields.Many2one('res.users', string=_('archives_creator'), required=True, default=lambda
-        self: self.env.user)
-    archives_creator_date = fields.Date()
 
     description = fields.Text()
 
@@ -25,7 +24,7 @@ class repair_quality(models.Model):
     state = fields.Selection([
         ('use', _('Use')),
         ('archive', _("Archive")),
-    ], default='use', string=_('workflow_state'))
+    ], default='use', string=_('archive workflow state'))
 
     @api.multi
     def action_to_default(self):
@@ -42,5 +41,5 @@ class repair_quality(models.Model):
             自动生成订单号：前缀WXD+序号
         """
         if vals.get('archvies_id', ' ') == ' ':
-            vals['archvies_id'] = self.env['ir.sequence'].next_by_code('srp.repair_quality') or '/'
+            vals['archvies_id'] = self.env['ir.sequence'].next_by_code('srp_repair_quality') or '/'
         return super(repair_quality, self).create(vals)
