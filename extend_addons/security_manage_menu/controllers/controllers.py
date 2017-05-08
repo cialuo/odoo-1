9527bf349c0bbd60a5bfc89144ef667012277ffa
xiangleilei <leilei.xiang@lantaiyuan.com>
2017-05-08 15:52:57 +0800

    d

--- extend_addons/security_manage_menu/controllers/controllers.py
+++ extend_addons/security_manage_menu/controllers/controllers.py
@@ -2,19 +2,19 @@
 from odoo import http
 
 # class SecurityManageMemu(http.Controller):
-#     @http.route('/security_manage_memu/security_manage_memu/', auth='public')
+#     @http.route('/security_manage_menu/security_manage_menu/', auth='public')
 #     def index(self, **kw):
 #         return "Hello, world"
 
-#     @http.route('/security_manage_memu/security_manage_memu/objects/', auth='public')
+#     @http.route('/security_manage_menu/security_manage_menu/objects/', auth='public')
 #     def list(self, **kw):
-#         return http.request.render('security_manage_memu.listing', {
-#             'root': '/security_manage_memu/security_manage_memu',
-#             'objects': http.request.env['security_manage_memu.security_manage_memu'].search([]),
+#         return http.request.render('security_manage_menu.listing', {
+#             'root': '/security_manage_menu/security_manage_menu',
+#             'objects': http.request.env['security_manage_menu.security_manage_menu'].search([]),
 #         })
 
-#     @http.route('/security_manage_memu/security_manage_memu/objects/<model("security_manage_memu.security_manage_memu"):obj>/', auth='public')
+#     @http.route('/security_manage_menu/security_manage_menu/objects/<model("security_manage_menu.security_manage_menu"):obj>/', auth='public')
 #     def object(self, obj, **kw):
-#         return http.request.render('security_manage_memu.object', {
+#         return http.request.render('security_manage_menu.object', {
 #             'object': obj
 #         })
