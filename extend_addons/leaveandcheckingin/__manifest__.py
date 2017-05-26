b755b57bc7307bcb75777c1b419a7b50e75c069a
tuxinglong <xianglong.tu@lantaiyuan.com>
2017-05-26 17:51:44 +0800

    暂存

--- extend_addons/leaveandcheckingin/__manifest__.py
+++ extend_addons/leaveandcheckingin/__manifest__.py
@@ -1,35 +1,35 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "leaveandcheckingin",
+    'name': "假期与考勤",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        假期与考勤""",
 
     'description': """
-        Long description of module's purpose
+        假期与考勤
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "深圳市蓝泰源信息技术股份有限公司",
+    'website': "http://www.lantaiyuan.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Optional Edition',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','hr_holidays'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'views/leaveandcheckingin.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
+    'installable': True,
+    'application': True,
 }
--- extend_addons/leaveandcheckingin/models/models.py
+++ extend_addons/leaveandcheckingin/models/models.py
@@ -2,3 +2,32 @@
 
 from odoo import models, fields, api, _
 from datetime import datetime
+
+# 休假与考勤
+
+class attence(models.Model):
+    _name = 'employee.attence'
+
+
+    employee_id = fields.Many2one('he.employee', string='employee')
+
+    # 打卡日期
+    recorddate = fields.Date(string='record date')
+
+
+class attencededucted(models.Model):
+    """
+    考勤扣款
+    """
+    _name = 'employee.attencededucted'
+
+    employee_id = fields.Many2one('he.employee', string='employee')
+
+    # 考勤月份
+    month = fields.Date(string='attence month')
+
+    # 缺勤时长 分钟单位
+    absence = fields.Integer(string='attence absence')
+
+    # 扣款金额
+    deducted = fields.Integer(string='deducted money')
