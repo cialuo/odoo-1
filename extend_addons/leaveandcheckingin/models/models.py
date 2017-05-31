f2c0e159b04338b845350c09f384e1dcfdbcd1b6
tuxinglong <xianglong.tu@lantaiyuan.com>
2017-05-31 10:41:45 +0800

    考勤记录

--- extend_addons/leaveandcheckingin/models/models.py
+++ extend_addons/leaveandcheckingin/models/models.py
@@ -6,13 +6,29 @@ from datetime import datetime
 # 休假与考勤
 
 class attence(models.Model):
-    _name = 'employee.attence'
+    """
+    考勤记录
+    """
+    _name = 'employee.attencerecords'
+    _rec_name = 'employee_id'
 
+    employee_id = fields.Many2one('hr.employee', string='employee')
 
-    employee_id = fields.Many2one('he.employee', string='employee')
+    # 上班打卡时间
+    checkingin= fields.Datetime(string='checkingin time')
 
-    # 打卡日期
-    recorddate = fields.Date(string='record date')
+    # 下班打卡时间
+    checkinginout = fields.Datetime(string='checkingout time')
+
+    # 缺勤时长
+    length = fields.Integer(string="time length")
+
+    # 状态
+    status = fields.Selection([
+        ('late','late'),
+        ('early','early'),
+        ('late+early','late+early')
+    ],string='status')
 
 
 class attencededucted(models.Model):
@@ -20,8 +36,9 @@ class attencededucted(models.Model):
     考勤扣款
     """
     _name = 'employee.attencededucted'
+    _rec_name = 'employee_id'
 
-    employee_id = fields.Many2one('he.employee', string='employee')
+    employee_id = fields.Many2one('hr.employee', string='employee')
 
     # 考勤月份
     month = fields.Date(string='attence month')
