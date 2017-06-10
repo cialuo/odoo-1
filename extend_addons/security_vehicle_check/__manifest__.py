242ebf902c272d778e6c645991c662bf83388252
xiangleilei <leilei.xiang@lantaiyuan.com>
2017-06-10 15:43:14 +0800

    修改安全 文档 车辆模块的权限

--- extend_addons/security_vehicle_check/__manifest__.py
+++ extend_addons/security_vehicle_check/__manifest__.py
@@ -12,7 +12,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['security_manage_menu', 'vehicle_manage'],
+    'depends': ['security_manage_menu', 'vehicle_manage','vehicle_group','materials_product'],
 
     # always loaded
     'data': [
