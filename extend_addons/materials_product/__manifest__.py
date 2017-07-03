2ff83365d8546e87ec547f3104d059ab8ce88bd3
xiao <mingmeng.xiao@lantaiyuan.com>
2017-07-03 15:04:00 +0800

    version update

--- extend_addons/materials_product/__manifest__.py
+++ extend_addons/materials_product/__manifest__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name': '物资管理',
-    'version': '1.0',
+    'version': '1.1',
     'category': 'Basic Edition',
     'summary': '物资管理',
     'author': 'Xiao',
