ad2f270b4524a0b72e8f7dbdb429f39d0db2d3b0
xiao <mingmeng.xiao@lantaiyuan.com>
2017-07-11 10:56:42 +0800

    fix erp-245 & erp-239

--- extend_addons/purchase_extend/__manifest__.py
+++ extend_addons/purchase_extend/__manifest__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name': '采购管理',
-    'version': '1.0',
+    'version': '1.1',
     'category': 'Basic Edition',
     'summary': '采购管理',
     'author': 'Xiao',
