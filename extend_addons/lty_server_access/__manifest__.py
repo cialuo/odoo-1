5b0d6dfaaa88e5a773db9a2e01e1fad37841f2ca
lihaihe <lihaihe@hotmail.com>
2017-07-17 19:39:15 +0800

    add lty_server_access module

---
+++ extend_addons/lty_server_access/__manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "lty_server_access",
+
+    'summary': """
+                            平台接入
+    """,
+
+    'description': """
+                        平台接入
+    """,
+
+    'author': "lihaihe",
+    'website': "http://www.lantaiyuan.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','operation_menu'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/cloud_server_config.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
---
+++ extend_addons/lty_server_access/models/cloud_server_config.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class lty_cloud_server_config(models.Model):
+    _name = 'lty.cloud.server.config'
+    _inherit = 'res.config.settings'
+
+    @api.model_cr
+    def get_latest_config(self):
+        self._cr.execute('select name,port ,access_key from lty_cloud_server_config where id = (select max(id)  from lty_cloud_server_config)')
+        dates = self._cr.fetchone()                
+        return dates
+    def get_ip_address(self):
+        if self.get_latest_config() :
+            return self.get_latest_config()[0]
+    def get_port(self):
+        if self.get_latest_config():
+            return self.get_latest_config()[1]
+    def get_access_key(self):
+        if self.get_latest_config() :
+            return self.get_latest_config()[2]    
+
+
+    name = fields.Char('IP Address', required=False, default = get_ip_address )
+    port = fields.Char('Port', required=False, default = get_port)
+    access_key = fields.Char('Access Key', required=False, default = get_access_key)
+
