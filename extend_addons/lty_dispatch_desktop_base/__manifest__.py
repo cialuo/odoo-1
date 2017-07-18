4dc150a6deb7a4cd8eb23c09dd851605dc1cb5d6
lihaihe <haihe.li@lantaiyuan.com>
2017-07-18 15:02:33 +0800

    User can define desktop setting on backend and open desktop in kanban view.

--- extend_addons/lty_dispatch_desktop_base/__manifest__.py
+++ extend_addons/lty_dispatch_desktop_base/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['dispatch_monitor_menu'],
+    'depends': ['web_kanban','dispatch_monitor_menu'],
 
     # always loaded
     'data': [
--- extend_addons/lty_dispatch_desktop_base/models/models.py
+++ extend_addons/lty_dispatch_desktop_base/models/models.py
@@ -2,14 +2,29 @@
 
 from odoo import models, fields, api
 
-# class lty_dispatch_desktop_base(models.Model):
-#     _name = 'lty_dispatch_desktop_base.lty_dispatch_desktop_base'
+class dispatch_control_desktop(models.Model):
+    _name = 'dispatch.control.desktop'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+    name = fields.Char()
+    value = fields.Integer()
+    description = fields.Text()
+    user = fields.Many2one('res.users', default=lambda self: self.env.user.id)
+    is_default = fields.Boolean(default=False)
+    component_ids = fields.One2many('dispatch.control.desktop.component', 'desktop_id',  string="Desktop's components")
+    
+    def open_dispatch_desktop(self):
+        return {
+            'name': 'Desktop',
+            'target': 'fullscreen',
+            'tag' : 'dispatch_desktop.page',
+            'type': 'ir.actions.client',
+        }
+           
+class dispatch_control_desktop_component(models.Model):
+    _name = 'dispatch.control.desktop.component'
+
+    name = fields.Char()
+    position_z_index = fields.Char()
+    position_top = fields.Char()
+    position_left = fields.Char()
+    desktop_id = fields.Many2one('dispatch.control.desktop', ondelete='restrict', string='Control Desktop')
