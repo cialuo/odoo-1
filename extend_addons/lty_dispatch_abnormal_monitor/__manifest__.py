53d9f5960fc414f2aecc540675e5e1f150902654
lihaihe <haihe.li@lantaiyuan.com>
2017-07-18 19:56:02 +0800

    Add Abnormal monitor menu

---
+++ extend_addons/lty_dispatch_abnormal_monitor/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Abnormal Monitor",
+
+    'summary': """
+        异常监控""",
+
+    'description': """
+        异常监控
+    """,
+
+    'author': "lihaihe",
+    'website': "http://www.lantaiyuan.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['dispatch_monitor_menu'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/abnormal_monitor_menu.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
