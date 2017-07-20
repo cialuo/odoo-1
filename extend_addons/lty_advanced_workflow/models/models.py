42b80279552005c32dc3402697a17d4c92b5714b
lihaihe <haihe.li@lantaiyuan.com>
2017-07-20 16:32:45 +0800

    审批设置建模

--- extend_addons/lty_advanced_workflow/models/models.py
+++ extend_addons/lty_advanced_workflow/models/models.py
@@ -1,15 +1,35 @@
-# -*- coding: utf-8 -*-
+    # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
 
-# class lty_advanced_workflow(models.Model):
-#     _name = 'lty_advanced_workflow.lty_advanced_workflow'
+class lty_advanced_workflow_cfg(models.Model):
+    _name = 'lty.advanced.workflow.cfg'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+    name = fields.Char()
+    code = fields.Char()
+    note = fields.Char()
+    model = fields.Many2one('ir.model')
+    start_status = fields.Char()
+    end_status = fields.Char()
+    company_id = fields.Many2one('res.company')
+    status = fields.Char()    
+    
+class lty_advanced_workflow_cfg_line(models.Model):
+    _name = 'lty.advanced.workflow.cfg.line'
+
+    squence = fields.Integer()
+    name = fields.Char()
+    conditions = fields.Text()
+    approve_type = fields.Char()
+    approve_posts = fields.Char()
+    approved_nubmber = fields.Char()
+    next_node = fields.Char()
+    node_type = fields.Char()
+    note = fields.Char()
+    status = fields.Char()	
+
+
+
+    @api.depends('value')
+    def _value_pc(self):
+        self.value2 = float(self.value) / 100
