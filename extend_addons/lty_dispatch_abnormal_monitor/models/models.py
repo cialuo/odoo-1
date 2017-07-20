8a0d14b430c31146a0d1e7ee8ea65a28906f127a
lihaihe <haihe.li@lantaiyuan.com>
2017-07-20 20:40:11 +0800

    将异常类别管理模型，由lty_dispatch_abnormal_monitor移至lty_dispatch_jobs模块

--- extend_addons/lty_dispatch_abnormal_monitor/models/models.py
+++ extend_addons/lty_dispatch_abnormal_monitor/models/models.py
@@ -1,10 +1,3 @@
 ﻿# -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-
-
-class lty_dispatch_abnorma_categ(models.Model):
-    _name = 'lty.dispatch.abnorma.categ'
-
-    name = fields.Char(required='1')
-    code = fields.Char(required='1')   
