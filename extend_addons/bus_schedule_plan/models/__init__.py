8ade9c56e0878b3b9e4d1a77cf8ad2bbefe608f5
xiangleilei <leilei.xiang@lantaiyuan.com>
2017-07-21 16:21:22 +0800

    行车计划表结构

--- extend_addons/bus_schedule_plan/models/__init__.py
+++ extend_addons/bus_schedule_plan/models/__init__.py
@@ -1,3 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from . import models
+from . import bus_date_type
+from . import bus_shift
+from . import bus_algorithm
+from . import bus_group
+from . import bus_staff_group
