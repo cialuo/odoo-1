b9bcac56f1a2eef90d9cacec5b0a031cc19e3d39
xiangleilei <leilei.xiang@lantaiyuan.com>
2017-07-21 17:17:05 +0800

    施救车队

---
+++ extend_addons/scheduling_parameters/models/rescue_fleet.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+
+
+class RescueFleet(models.Model):
+    _name = 'rescue_fleet'
+
+    company = fields.Char(required=True)
+    vehicle_no = fields.Char(required=True)
+    responsible_person = fields.Char(required=True)
+    responsible_telephone = fields.Char()
+    stop_address = fields.Char(required=True)
+    service_route = fields.Char()
+    service_date = fields.Datetime()
+
