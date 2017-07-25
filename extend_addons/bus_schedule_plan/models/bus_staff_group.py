7b60370357e18689c25435b86656324756889dc3
xiangleilei <leilei.xiang@lantaiyuan.com>
2017-07-25 09:50:35 +0800

    人车配班表的修改

--- extend_addons/bus_schedule_plan/models/bus_staff_group.py
+++ extend_addons/bus_schedule_plan/models/bus_staff_group.py
@@ -9,6 +9,11 @@ class BusStaffGroup(models.Model):
     """
     _name = 'bus_staff_group'
     name = fields.Char()
+    route_id = fields.Many2one('route_manage.route_manage', required=True)
+    bus_algorithm_id = fields.Many2one('bus_algorithm', required=True)
+    bus_driver_algorithm_id = fields.Many2one('bus_driver_algorithm', required=True)
+
+    group_line_ids = fields.One2many('bus_staff_group_line', 'staff_group_id')
 
 
 class BusStaffGroupLine(models.Model):
@@ -17,4 +22,11 @@ class BusStaffGroupLine(models.Model):
     """
     _name = 'bus_staff_group_line'
     name = fields.Char()
+    staff_group_id = fields.Many2one('bus_staff_group')
+
+    vehicle_id = fields.Many2one('fleet.vehicle', string="Vehicle No", help='Vehicle No', required=True)
+    state = fields.Selection(related='vehicle_id.state', readonly=True)
+
+    operation_state = fields.Selection([('operation', "operation"),('flexible', "flexible")], default='operation',
+                                        required=True)
 
