f6dc10d8da1ec2e73f5e6d37a9b0e869236c9f1b
xiao <mingmeng.xiao@lantaiyuan.com>
2017-07-28 19:32:03 +0800

    fix erp-440

---
+++ extend_addons/materials_product_data/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': u'物资基础数据',
+    'version': '1.0',
+    'category': 'Basic Edition',
+    'summary': '',
+    'author': 'Xiao',
+    'description': """
+    """,
+    'data': [
+        'data/product.uom.csv',
+        # 'data/product.category.csv',
+    ],
+    'depends': ['materials_product'],
+}
