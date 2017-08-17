e46cb696cbef742406403044c795a4d77e287d44
chen jie <46638448@qq.com>
2017-08-17 17:01:56 +0800

    Merge branch 'develop-dispatch' of http://202.104.136.228:8001/OdooDispatch/Odoo10 into develop-dispatch
    # Conflicts:
    #	extend_addons/lty_dispatch_desktop/static/src/js/websocket.js

--- extend_addons/bus_schedule_plan/static/src/js/operation_plan.js
+++ extend_addons/bus_schedule_plan/static/src/js/operation_plan.js
@@ -3,41 +3,56 @@ odoo.define('abc', function (require) {
     var Model = require('web.Model');
     var model = new Model("scheduleplan.busmovetime");
     var recid = $('#the_rec_id .o_form_field').text();
-    console.log(recid);
-    model.call("reoppaln2web",[recid]).then(function (data) {
-        console.log(data);
+    model.call("reoppaln2web", [recid]).then(function (data) {
+        console.log(JSON.parse(data))
+        var a = JSON.parse(data).bus;
+        console.log(a[1])
+        for(var ts=0;ts<a[1].length;ts++){
+            $('.sort_title').append('<li><div>'+ts+'</div><div>'+a[1][ts][2]+'</div></li>')
+        }
+        alert(Object.keys(a).length)
+        for(var bn = 0;bn<Object.keys(a).length;bn++){
+
+        }
+        for (var i in a) {
+            console.log(a[i].length);
+        }
     });
-
-
     // var arr = [];
-    // for (var i = 0, m = $('.abcde li').length; i < m; i++) {
-    //     if ($('.abcde li').eq(i).html() != '') {
-    //         arr.push($('.abcde li').eq(i).html());
+    // for (var i = 0, m = $('.sort_top li').length; i < m; i++) {
+    //     if ($('.sort_top li').eq(i).html() != '') {
+    //         arr.push($('.sort_top li').eq(i).html());
     //     }
     // }
-    var arr = [1, 2, 3, 6, 5, 4, 7, 8, 9, 3, 1, 2, 3, 6, 11, 15];
-    $('body').on('dblclick', '.abcd .abcde li', function () {
-        if (!$(this).hasClass('abc') && $(this).html() != '') {
-            if ($('.abcde li.abc').length % 4 == 0) {
-                $('.abcd').append('<ul class="abcde"><li></li><li></li><li></li><li></li></ul>')
-                var a = parseInt($('.title1 li').last().find('div').html()) + 1;
-                $('.title1').append('<li><div>' + a + '</div><div>竹-竹</div></li>');
+
+    function move() {
+        $('.opera_sort').on('dblclick', '.sort_top li', function () {
+            if (!$(this).hasClass('sort_out') && $(this).html() != '') {
+                if ($('.sort_top li.sort_out').length % 4 == 0) {
+                    $('.opera_sort').append('<ul class="sort_top"><li></li><li></li><li></li><li></li></ul>')
+                    var a = parseInt($('.sort_title li').last().find('div').html()) + 1;
+                    $('.sort_title').append('<li><div>' + a + '</div><div>竹-林</div></li>');
+                }
+                $(this).css('background', 'red').html('').addClass('sort_out');
+                for (var i = 0; i < arr.length; i++) {
+                    $(".sort_top li:not(.sort_out)").eq(i).html(arr[i]);
+                }
+            } else if ($(this).hasClass('sort_out')) {
+
+                if ($('.sort_top  li.sort_out').length % 4 == 1) {
+                    $('.opera_sort').find('ul:last').remove();
+                    $('.sort_title').find('li:last').remove();
+                }
+                $(this).css('background', 'white').removeClass('sort_out');
+                $(".sort_top li:not(.sort_out)").html('');
+                for (var i = 0; i < arr.length; i++) {
+                    $(".sort_top li:not(.sort_out)").eq(i).html(arr[i]);
+                }
             }
-            $(this).css('background', 'red').html('').addClass('abc');
-            for (var i = 0; i < arr.length; i++) {
-                $(".abcde li:not(.abc)").eq(i).html(arr[i]);
-            }
-        } else if ($(this).hasClass('abc')) {
-            if ($('.abcde  li.abc').length % 4 == 1) {
-                $('.abcd').find('ul:last').remove();
-                $('.title1').find('li:last').remove();
-            }
-            $(this).css('background', 'white').removeClass('abc');
-            $(".abcde li:not(.abc)").html('');
-            for (var i = 0; i < arr.length; i++) {
-                $(".abcde li:not(.abc)").eq(i).html(arr[i]);
-            }
-        }
-    })
+            return false;
+        });
+    }
+
+    move();
     // model_students.call('get_examination_info').then(function (data) {
 })
