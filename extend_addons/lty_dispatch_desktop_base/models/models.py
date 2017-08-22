033162ffc9b932a300a7cf150aeba4bec3f27405
lihaihe <lihaihe@hotmail.com>
2017-08-22 16:50:04 +0800

    备注缩进

--- extend_addons/lty_dispatch_desktop_base/models/models.py
+++ extend_addons/lty_dispatch_desktop_base/models/models.py
@@ -31,9 +31,9 @@ class dispatch_control_desktop_component(models.Model):
     position_left = fields.Char()
     line_id = fields.Many2one('route_manage.route_manage')
     desktop_id = fields.Many2one('dispatch.control.desktop', ondelete='restrict', string='Control Desktop')
-	#备注
+    #备注
     remark = fields.Char()
-	#显示顺序
+    #显示顺序
     show_order = fields.Integer()
-	#设备号
+    #设备号
     on_bord_id = fields.Integer()
