8a968c19d0b657d57fe55bcfaf4d026165b783bb
lihaihe <lihaihe@hotmail.com>
2017-09-01 08:57:48 +0800

    增加依赖关系

--- extend_addons/lty_advanced_workflow/__manifest__.py
+++ extend_addons/lty_advanced_workflow/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['employees','mail','product'],
+    'depends': ['employees','mail','product','stock','purchase'],
 
     # always loaded
     'data': [
