2f745185d61bdf1ed020a21284ff4033d54e7188
xiangleilei <leilei.xiang@lantaiyuan.com>
2017-09-01 14:08:53 +0800

    运营管理，调度监控 公用权限组

---
+++ extend_addons/schedule_dispatch_group/__manifest__.py
@@ -0,0 +1,27 @@
+# -*-coding:UTF-8-*-
+
+{
+    'name': "运营-调度 公用权限组",
+
+    'summary': """
+        运营-调度 公用权限组
+        """,
+    'description': """
+        a)运营-调度 公用权限组(运营专员，运营经理，调度员)
+    """,
+
+    'author': "Xiang",
+    'website': "http://www.lantaiyuan.com/",
+
+    'category': 'Basic Edition',
+    'version': '0.1',
+
+    'depends': ['base'],
+
+    'data': [
+        'data/schedule_dispatch_group.xml',
+        'security/schedule_dispatch_group.xml',
+    ],
+    'installable': True,
+    'application': True
+}
