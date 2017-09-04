a4893ec87ed7ef222849c63fb06b84bb589d9d6f
xiao <mingmeng.xiao@lantaiyuan.com>
2017-09-04 15:31:17 +0800

    car resource

--- extend_addons/lty_dispatch_restful/models/mapping.py
+++ extend_addons/lty_dispatch_restful/models/mapping.py
@@ -518,6 +518,7 @@ op_dispatchplan = {
     ('workDate', 'work_date'): None,
     #行车规则ID 后台获取
     ('linePlanId', 'rule_id'): None,
+    #
 }
 
 #1.3.15	车辆资源
@@ -537,7 +538,7 @@ op_busresource = {
     #台次 add
     ('orderNo', 'arrangenumber'): None,
     #车辆状态 add
-    ('carStateId', 'car_state'): {'zc': 1001, 'jd': 2008},
+    ('carStateId', 'workstatus'): {'operation': 1001, 'flexible': 2008},
     #方向 add
     ('direction', 'direction'): None,
     #工作日期date 后台获取，
