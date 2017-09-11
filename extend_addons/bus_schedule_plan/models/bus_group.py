f594c1eb2a7f77ed6c735a176f04ee9b45ab28cd
xiangleilei <leilei.xiang@lantaiyuan.com>
2017-09-11 10:39:45 +0800

    修改定时任务

--- extend_addons/bus_schedule_plan/models/bus_group.py
+++ extend_addons/bus_schedule_plan/models/bus_group.py
@@ -467,9 +467,10 @@ class BusGroupDriverVehicleShift(models.Model):
     def run_scheduler(self):
         """
         班组管理 定时任务的入口
-        :return:use_date 指 执行时间的前一天 如2017-09-08的计划，use_date：2017-09-07
+        :return:
         """
         _logger.info(u'Start run_scheduler')
-        use_date = datetime.datetime.strftime(datetime.date.today() - timedelta(days=1), "%Y-%m-%d")
-        self.scheduler_vehicle_shift(use_date=use_date)
+        # use_date = datetime.datetime.strftime(datetime.date.today() - timedelta(days=1), "%Y-%m-%d")
+        # self.scheduler_vehicle_shift(use_date=use_date)
+        self.scheduler_vehicle_shift()
         _logger.info(u'End run_scheduler')
