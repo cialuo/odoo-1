ed81cba4933c861a2ba9a44fbf7ba0175fb18d44
xiao <mingmeng.xiao@lantaiyuan.com>
2017-09-13 16:15:35 +0800

    fix wkf merger develop-wkf update

--- extend_addons/lty_advanced_workflow/models/__init__.py
+++ extend_addons/lty_advanced_workflow/models/__init__.py
@@ -4,4 +4,7 @@ from . import approve_cfg
 from . import approve_center
 from . import models_support
 from . import stock_picking_support
+from . import purchase_order_support
+from . import purchase_plan_support
+from . import stock_move_support
 import res_user
