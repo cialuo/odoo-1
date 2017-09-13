13b34859c4373d66c2d95ffe4d37a1e64326cd77
lihaihe <lihaihe@hotmail.com>
2017-09-13 13:47:59 +0800

    完善取消审批和重新发起审批

--- extend_addons/lty_advanced_workflow/models/approve_center.py
+++ extend_addons/lty_advanced_workflow/models/approve_center.py
@@ -21,19 +21,49 @@ class lty_approve_center_group(models.Model):
     status = fields.Selection([
             ('approving', u'审批中'),
             ('rejected', u'拒绝'),
+            ('cancel', u'取消'),
             ('done', u'完成')
         ], string=u'状态', required=True, track_visibility='always', default='approving')
     
     @api.multi
     def do_reset(self):
-        if  self.status == 'rejected'  :
+        if  self.status != 'done' :
+            self.env['lty.approve.center'].search([('center_id','=',self.id)])
             for node in self.center_id :
                 node.unlink()
+            for node_false in self.env['lty.approve.center'].search([('center_id','=',self.id),('active','=',False)]) :
+                node_false.unlink()
         else:
-            raise UserError((u'只有被拒绝的审批才充许重置!. '))
+            raise UserError((u'该流程已审批完成，禁止取消!. '))
 
-        self.sudo().object_id.write({'approve_state': u'审批被重置'})  
+        self.sudo().object_id.write({'approve_state': u'审批被取消'})
+        self.sudo().write({'status': 'cancel'})  
         #self.write({'status': 'approved','approve_opinions': ''})    
+
+    @api.multi
+    def do_reopen(self):
+        self.do_reset()
+        productid = self.object_id
+        cfg_id =  self.cfg_id.id
+        for cfg_line in self.env['lty.advanced.workflow.cfg'].browse(cfg_id).line_ids :
+            # print cfg_line
+            val_dict = {
+                'name': self.env['lty.advanced.workflow.cfg'].browse(cfg_id).code + '-' + productid.name + '-'+ str(cfg_line.squence),  
+                'description':self.env['lty.advanced.workflow.cfg'].browse(cfg_id).name,                  
+                'object_id': productid._name + ',' +str(productid.id), 
+                'approve_node':cfg_line.name,  
+                'status':'commited',  
+                'cfg_line_id':cfg_line.id,
+                'cfg_father_line_id':cfg_line.farther_node.id,
+                #'approve_posts': [(6,0,cfg_line.approve_posts.ids)],
+                'approve_post': cfg_line.approve_post.id,
+                'start_user': self.env.user.id,
+                'center_id': self.id,
+                
+            }
+            self.env['lty.approve.center'].sudo().create(val_dict)
+
+        self.sudo().write({'status': 'approving'})  
     
     
 class lty_approve_center(models.Model):
@@ -106,7 +136,7 @@ class lty_approve_center(models.Model):
             
             if user.cfg_line_id.farther_node :
                 object2 = user.object_id._name+','+str(user.object_id.id)
-                farther_node_state = self.sudo().search([('cfg_line_id', '=',user.cfg_line_id.farther_node.id),('object_id', '=',object2)]).approved
+                farther_node_state = self.sudo().search([('cfg_line_id', '=',user.cfg_line_id.farther_node.id),('object_id', '=',object2)],limit = 1).approved
             else:
                 farther_node_state = True                            
             domain = eval( user.cfg_line_id.conditions)
@@ -150,9 +180,9 @@ class lty_approve_center(models.Model):
             raise UserError((u'审批节点未被活活!. '))             
         else:
             for next_node in self.search([('active', '=',False),('cfg_father_line_id', '=',self.cfg_line_id.id),('object_id', '=',self.object_id._name+','+str(self.object_id.id))]):
-                #todo 这里需要判断下，节点人数
+                #todo 这里需要判断下节点人数
                 next_node.write({'active': False})
-                next_node.sudo().center_id.write({'status': 'rejected'})                                
+                #next_node.sudo().center_id.write({'status': 'rejected'})                                
             val_dict = {
                 'center_id': self.id,
                 'user_id':self.env.user.id,
@@ -161,6 +191,8 @@ class lty_approve_center(models.Model):
             }
             self.env['lty.approve.logs'].create(val_dict)            
             self.write({'status': 'rejected','approve_opinions': ''})
+            #更新当前节点对应的组状态为拒绝
+            self.sudo().center_id.write({'status': 'rejected'})                                
         self.sudo().object_id.write({'approve_state': u'审批节点'+':'+self.approve_node + u'拒绝'})  
 class lty_approve_logs(models.Model):
     _name = 'lty.approve.logs'
