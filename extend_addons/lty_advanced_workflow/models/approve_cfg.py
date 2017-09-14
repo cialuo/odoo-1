a56c92a866e189cca052af2370a87d965f51a8bf
xiao <mingmeng.xiao@lantaiyuan.com>
2017-09-14 09:19:04 +0800

    fix erp-590 591 592

--- extend_addons/lty_advanced_workflow/models/approve_cfg.py
+++ extend_addons/lty_advanced_workflow/models/approve_cfg.py
@@ -1,6 +1,8 @@
     # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError
+
 
 class lty_advanced_workflow_cfg(models.Model):
     _name = 'lty.advanced.workflow.cfg'
@@ -29,7 +31,13 @@ class lty_advanced_workflow_cfg(models.Model):
     
     def do_confirm(self):    
         self.write({'status': 'commited'})
-    def do_approve(self):    
+    def do_approve(self):
+        for line in self.line_ids :
+            try:
+                self.env[line.cfg_id.model.model].search(eval(line.conditions),limit=1)
+            except Exception,e:
+                raise UserError((line.conditions+u'domain表达式错误!'))
+                    
         self.write({'status': 'approved'})    
     def do_cancel(self):    
         self.write({'status': 'draft'})        
@@ -49,7 +57,7 @@ class lty_advanced_workflow_cfg_line(models.Model):
     approve_posts = fields.Many2many('employees.post', 'lty_wkf_cfg_line_post', 'post_id', 'approve_posts', 'Approve Post', help="")
     approve_post = fields.Many2one('employees.post','Approve Post', help="")
     approved_nubmber = fields.Char(default='1',readonly='1')
-    farther_node = fields.Many2one('lty.advanced.workflow.cfg.line')
+    farther_node = fields.Many2one('lty.advanced.workflow.cfg.line', domain="[('cfg_id','=',cfg_id)]")
     next_node = fields.Many2one('lty.advanced.workflow.cfg.line')
     node_type = fields.Selection([
             ('start', 'start'),
@@ -61,10 +69,28 @@ class lty_advanced_workflow_cfg_line(models.Model):
             ('open', 'open'),
             ('close', 'close')
         ], string='status', required=True, default='open')
-    cfg_id = fields.Many2one('lty.advanced.workflow.cfg')    
+    cfg_id = fields.Many2one('lty.advanced.workflow.cfg',ondelete='cascade')    
 
-
-
+    @api.onchange('conditions')
+    def onchange_conditions(self):
+        if self.cfg_id.model.model :
+            try:
+                self.env[self.cfg_id.model.model].search(eval(self.conditions),limit=1)
+            except Exception,e:
+                self.conditions = '' 
+                raise UserError((u'domain表达式错误！'+e.message))
+                return  {'warning': (e.message)}          
+            
+        #=======================================================================
+        # if not self.product_id or self.product_qty < 0.0:
+        #     self.product_qty = 0.0
+        # if self.product_qty < self._origin.product_qty:
+        #     return {'warning': _("By changing this quantity here, you accept the "
+        #                          "new quantity as complete: Odoo will not "
+        #                          "automatically generate a back order.")}
+        #=======================================================================
+    
+    
     @api.depends('value')
     def _value_pc(self):
         self.value2 = float(self.value) / 100
