ac6376189f8367224372371abf5f0013c4fd373c
jie.he <jie.he@lantaiyuan.com>
2017-09-15 17:52:44 +0800

    fix ERPKP-7 库存预警生成采购计划

--- extend_addons/stock_warning/__manifest__.py
+++ extend_addons/stock_warning/__manifest__.py
@@ -12,9 +12,10 @@
     'website': "http://www.lantaiyuan.com/",
     'category': 'Optional Edition',
     'version': '0.1',
-    'depends': ['base','stock_extend'],
+    'depends': ['base','stock_extend','purchase_plan'],
     'data': [
         # 'security/ir.model.access.csv',
+        'views/stock_warning_data.xml',
         'views/report_search_wizard.xml',
         'views/warning_report.xml',
         'views/menus.xml',
--- extend_addons/stock_warning/report/stock_warning.py
+++ extend_addons/stock_warning/report/stock_warning.py
@@ -1,6 +1,6 @@
-
-from odoo import api, models
-
+# -*- coding: utf-8 -*-
+from odoo import api, models, fields, _,exceptions
+import time
 class StockWarning(models.AbstractModel):
 
     _name = 'report.stock_warning.warning_report'
@@ -17,4 +17,61 @@ class StockWarning(models.AbstractModel):
         }
         return report_obj.render('stock_warning.warning_report', docargs)
 
+class StockWarningData(models.Model):
 
+    _name = 'stock.warning_data'
+    _description = 'Stock Warning Data'
+
+    warehouse = fields.Many2one('stock.warehouse')
+
+    product = fields.Many2one('product.product')
+
+    qty_available = fields.Float()
+
+    virtual_available = fields.Float()
+
+    product_min_qty = fields.Float()
+
+    @api.multi
+    def action_create_purchase_plan(self):
+        '''
+            创建采购计划
+        :return:
+        '''
+        context = dict(self._context or {})
+        ids = context['active_ids']
+        if ids:
+            warning_data = self.env['stock.warning_data'].search([('id','in',ids)])
+            if warning_data:
+
+                '''创建采购计划'''
+                user = self.env.user
+                purchase_plan_val = {
+                    'user_id':user.employee_ids[0].id,
+                    'month':time.strftime("%Y-%m")
+                }
+                plan = self.env['purchase.plan'].create(purchase_plan_val)
+
+                '''创建采购计划详情'''
+                for warning in warning_data:
+
+                    #获取供应商
+                    p_order = self.env['purchase.order.line'].search([('product_id', '=', warning.product.id)], limit=1,
+                                                                     order='id desc')
+                    p_supplierinfo = self.env['product.supplierinfo'].search([('name', '=', p_order.partner_id.id)],
+                                                                             limit=1)
+                    val = {
+                        'plan_id':plan.id,
+                        'product_id':warning.product.id,
+                        'seller_id':p_supplierinfo.id,
+                        'product_tmpl_id':warning.product.product_tmpl_id.id,
+                        'price_unit':p_supplierinfo.price
+                    }
+                    self.env['purchase.plan.line'].create(val)
+
+        return {
+                'type': 'ir.actions.act_window',
+                'res_model': 'purchase.plan',
+                'res_id': plan.id,
+                'views': [[False, 'form']],
+                }
