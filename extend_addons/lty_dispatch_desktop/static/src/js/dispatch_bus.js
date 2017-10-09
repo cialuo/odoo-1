c6cc37f954dfbd85e453847a17a0289a38f53dad
chen jie <46638448@qq.com>
2017-10-09 14:32:22 +0800

    Merge branch 'develop-master' of http://202.104.136.228:8001/OdooDispatch/Odoo10 into develop-master
    # Conflicts:
    #	extend_addons/lty_dispatch_desktop/static/src/js/websocket.js

--- extend_addons/lty_dispatch_desktop/static/src/js/dispatch_bus.js
+++ extend_addons/lty_dispatch_desktop/static/src/js/dispatch_bus.js
@@ -176,9 +176,9 @@ odoo.define('lty_dispaych_desktop.getWidget', function (require) {
                                                         console.log(res)
                                                         for (var i = 0; i < res.length; i++) {
                                                             $('.run_car_hide').find('.line_car').attr('bus_no', res[i].onboard);
-                                                            if (res[i].onlineFlag == 1) {
+                                                            if (res[i].onlineFlag == 0) {
                                                                 $('.run_car_hide').find('.line_car').removeClass('to_gray');
-                                                            } else {
+                                                            } else if(res[i].onlineFlag == 1) {
                                                                 $('.run_car_hide').find('.line_car').addClass('to_gray');
                                                             }
                                                             if (res[i].direction == 0) {
