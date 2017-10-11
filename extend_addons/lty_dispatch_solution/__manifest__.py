02f3c4cbe4ab81788af3513176fc127704df21da
genghua.jiang <genghua.jiang@lantaiyuan.com>
2017-10-11 09:52:50 +0800

    新增 调度机器人 解决方案模块

---
+++ extend_addons/lty_dispatch_solution/__manifest__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "lty_dispatch_solution",
+
+    'summary': """
+        解决方案""",
+
+    'description': """
+        调度及机器人-解决方案
+    """,
+
+    'author': "jianggenghua",
+    'website': "http://www.lantaiyuan.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/dispatch_solution_views.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
